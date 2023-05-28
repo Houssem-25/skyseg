8a757bbf0959cf64571f7b6a1ec6b1d6042c2c1f
Boulahbal Houssem <boulahbalhoussem@mail.com>
2023-05-28 20:01:07 +0200

    evaluating on coco_dataset is working git status

---
+++ coco_dataloader.py
@@ -0,0 +1,28 @@
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+import json
+import os
+from glob import glob
+from utils import pngToCocoResult
+class CocoSemantic(Dataset):
+    def __init__(self, img_dir, mask_dir):
+        self.root_dir = img_dir
+        self.mask_dir = mask_dir
+        self.image_files = glob(self.root_dir +"/*")
+
+        self.image_files = sorted(self.image_files , key=lambda x: int(x.split(".")[0].split("/")[-1]))
+        self.masks_files = glob(self.mask_dir +"/*")
+        self.masks_files = sorted(self.masks_files, key=lambda x: int(x.split(".")[0].split("/")[-1]))
+        self.target_sky_color = [75,124,169]
+        self.to_tensor = transforms.ToTensor()
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        image = Image.open(self.image_files[idx]).convert('RGB')
+        mask = np.array(Image.open(self.masks_files[idx]))
+        mask = np.all(mask == self.target_sky_color, axis=-1)
+        return self.to_tensor(image), self.to_tensor(mask).float()
---
+++ evaluate_sky_coco.py
@@ -0,0 +1,56 @@
+import torch
+from coco_dataloader import CocoSemantic
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from model import SkySegmentation
+from options import Option
+from tqdm import tqdm
+import torch
+def downsample(tensor, factor=2):
+    w,h = tensor.size()[-2:]
+    return torch.nn.functional.interpolate(tensor, (w//factor,h//factor))
+
+@torch.no_grad()
+def evaluate(model, dataloader, class_id, single_model):
+    model.eval()
+    with torch.no_grad():
+        total_correct_1 = 0
+        total_correct_2 = 0
+        total_pixels = 0
+        i = 0
+        for images, labels in tqdm(dataloader):
+            images = downsample(images.cuda())
+            labels = downsample(labels.cuda()) == 1.0
+            if single_model:
+                mask_model_1 = model.segment_sky(images)
+            else:
+                mask_model_1,mask_model_2 = model.segment_sky(images)
+                correct = (mask_model_2 == labels).sum()
+                total_correct_2 += correct.item()
+
+            correct = (mask_model_1 == labels).sum()
+            total_correct_1 += correct.item()
+            total_pixels += labels.numel()
+        return total_correct_1 / total_pixels , total_correct_2 / total_pixels
+
+
+# Load your trained model
+if __name__ == "__main__":
+    # init model
+    opt = Option().parse()
+    eval_dataset = CocoSemantic("/home/houssem/PhD/datasets/COCO/val2017", "/home/houssem/PhD/datasets/COCO/panoptic_val2017")
+    device = torch.device(opt.device)
+    eval_dataloader = DataLoader(eval_dataset, batch_size=opt.batch_size, shuffle=False, num_workers=opt.num_workers)
+
+    model = SkySegmentation(opt)
+    class_id = model.sky_id_model_1
+
+    # Create the DataLoader for evaluation
+    batch_size = 1
+    num_workers = 4
+    eval_dataloader = DataLoader(eval_dataset, batch_size=opt.batch_size, shuffle=False, num_workers=opt.num_workers)
+
+    # Evaluate the model
+    accuracy_1, accuracy_2 = evaluate(model, eval_dataloader, class_id, opt.single_model)
+    print(f"Accuracy for class {class_id} using OR : {accuracy_1}")
+    print(f"Accuracy for class {class_id} using AND: {accuracy_2}")
